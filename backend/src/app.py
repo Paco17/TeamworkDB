67d4f1d98de1f0bcad6fc96bee3174032dd69603
Paco17 <A01636425@itesm.mx>
2022-03-09 13:38:58 -0600

    Initial commit

---
+++ backend/src/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost/pythonreactdb'
+mongo = PyMongo(app)
+
+db = mongo.db.users
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+if __name__ == "__main__":
+    app.run(debug=True)
