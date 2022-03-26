e3c38a57bcb2fbc420d6916349d788f219c559cd
Paco17 <A01636425@itesm.mx>
2022-03-25 18:44:02 -0600

    Create User done

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask_pymongo import PyMongo
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo, ObjectId
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,7 +10,41 @@ db = mongo.db.users
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return "Pagina Prueba"
+
+@app.route('/users', methods=['POST'])
+def createUser():
+    id = db.insert_one({
+        #Devuelve un id 
+        'name':request.json['name'],
+        'email': request.json['email'],
+        'password': request.json['password']
+        
+    })
+    return jsonify(str(ObjectId(id.inserted_id)))
+
+
+@app.route('/users', methods=['GET'])
+def getUsers():
+    users = []
+    for doc in db.find():
+        users.append({})
+    return 'received'
+
+@app.route('/users/<id>', methods=['GET'])
+def getUser():
+    print(request.json)
+    return 'received'
+
+@app.route('/users/<id>', methods=['GET'])
+def deleteUser():
+    print(request.json)
+    return 'deleted'
+
+@app.route('/users/<id>', methods=['PUT'])
+def updateUser():
+    print(request.json)
+    return 'updated'
 
 if __name__ == "__main__":
     app.run(debug=True)
